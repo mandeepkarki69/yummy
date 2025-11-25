910bd142c3b70509ce35cf5285a22a66633c44d6
mandeep69 <karkimandeep9@gmail.com>
2025-11-25 13:23:33 +0545

    test

--- app/core/database.py
+++ app/core/database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
+#this is database connection
 engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
