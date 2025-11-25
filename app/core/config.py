b1945d381f5f91b85900f03e359965e56e557bdc
mandeep69 <karkimandeep9@gmail.com>
2025-11-25 15:33:08 +0545

    changes config

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings  # updated import
 
 class Settings(BaseSettings):
-    # DATABASE_URL: str = "postgresql+asyncpg://mandeep:kxyneRmTzPVDSMzw19efk0GpmZ7Dkbmf@dpg-d4im8sa4d50c73d6rnl0-a.singapore-postgres.render.com/yummy_db_z8q4"
-    DATABASE_URL: str = 'postgresql+asyncpg://postgres:1234567890@localhost:5432/yummy_db'
+    DATABASE_URL: str = "postgresql+asyncpg://mandeep:kxyneRmTzPVDSMzw19efk0GpmZ7Dkbmf@dpg-d4im8sa4d50c73d6rnl0-a.singapore-postgres.render.com/yummy_db_z8q4"
+    # DATABASE_URL: str = 'postgresql+asyncpg://postgres:1234567890@localhost:5432/yummy_db'
     APP_NAME: str = "Yummy API"
     DEBUG: bool = True
 
