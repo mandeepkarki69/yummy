a5f808b2052612dffee9f162330a459b07cbe5b3
mandeep69 <karkimandeep9@gmail.com>
2025-11-25 14:39:34 +0545

    changes made new db

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings  # updated import
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://mandeep:kxyneRmTzPVDSMzw19efk0GpmZ7Dkbmf@dpg-d4im8sa4d50c73d6rnl0-a.singapore-postgres.render.com/yummy_db_z8q4?sslmode=require"
+    DATABASE_URL: str = "postgresql+asyncpg://mandeep:kxyneRmTzPVDSMzw19efk0GpmZ7Dkbmf@dpg-d4im8sa4d50c73d6rnl0-a.singapore-postgres.render.com/yummy_db_z8q4"
     APP_NAME: str = "Yummy API"
     DEBUG: bool = True
 
--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,20 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
+import ssl
 
-#this is database connection
-engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE 
+
+# Database connection
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    future=True,
+    connect_args={"ssl": ssl_context}
+)
+
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
