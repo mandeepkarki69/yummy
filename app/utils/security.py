b22ddaeea7da71b15472cbedef9686d24fa9aa5a
mandeep69 <karkimandeep9@gmail.com>
2025-11-25 14:52:29 +0545

    password error fix 1

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,12 +1,11 @@
+# app/utils/security.py
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(
-    schemes=["bcrypt_sha256"],
-    deprecated="auto"
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    truncated_password = password[:72]  # truncate to 72 characters
+    return pwd_context.hash(truncated_password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
